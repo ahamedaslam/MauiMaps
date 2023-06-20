12911e14d85954d3855ad0887afc13fc81538333
Ahamed Aslam <87639294+ahamedaslam@users.noreply.github.com>
2023-06-20 17:45:43 +0530

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿ namespace MuiMapsApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Maps;
+
+namespace MuiMapsApp;
+
+public partial class MainPage : ContentPage
+{
+	//int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		var location = new Location(36.9628066, -122.0194722);
+		var mapSpan = new MapSpan(location, 0.01, 0.01);
+		map.MoveToRegion(mapSpan);
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		//count++;
+
+		//if (count == 1)
+		//	CounterBtn.Text = $"Clicked {count} time";
+		//else
+		//	CounterBtn.Text = $"Clicked {count} times";
+
+		//SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
